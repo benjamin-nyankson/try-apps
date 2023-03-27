f76a886b841a98edcde32bfd9ac7e5c42a4276b5
nyankson-benjamin <benjamin.nyankson@amalitech.com>
2023-03-27 13:26:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ import ImageTest from "./Pages/ImageTest";
 import ImageTest1 from "./Pages/ImageTest1";
 import FromDrive from "./Pages/UPLOAD/UploadFromDrive";
 import ImageLink from "./Pages/ImageLink";
-import OneDrive from "./Pages/OnedrivePicker";
 import DropzoneComponent from "./Pages/DropzoneTwo";
 import Dropzone2Component from "./Pages/DropZone3";
 import ImageCompressor from "./Pages/ImageCompressor";
@@ -27,7 +26,7 @@ import LoadingSinner from "./Pages/MUISkeleton";
 import Login from "./Pages/Login";
 import UploadFile from "./Pages/UploadFile";
 import UploadImgFile from "./Pages/UploadImagePage";
-// import Onedrive from "./Pages/OneDrive";
+
 import DropBox from "./Pages/Dropbox";
 import Users from "./Pages/Users/Users";
 import Testing from "./Pages/Testing";
@@ -39,8 +38,24 @@ import GrayScale from "./Pages/GrayScale";
 import ReactOtp from "./components/ReactOtp";
 import Paginate from "./components/Pagination/Paginate";
 import LoadingPage from "./LoadingButton/LoadingPage";
-
-
+import Movies from "./Movies/Movies";
+import DisabledButton from "./DisabledButton/DisabledButton";
+import UserList from "./DynamicPageRouting/UserList";
+import UserPage from "./DynamicPageRouting/userPage";
+import CardSlider from "./components/CardSlider";
+import MoviePage from "./components/MoviePage";
+import TitleExtraction from "./Pages/TitleExtraction";
+import MapWithIcons from "./components/MapWithIcons";
+import MapWithIconPage from "./components/MapWithIconPage";
+import MapIntoTable from "./components/MapIntoTable";
+import MapIntoTableUserPage from "./components/MapIntoTableUserPage";
+import Quiz from "./Quiz/Quiz";
+import Quiz1 from "./Quiz/Quiz1";
+import FormWithoutMap from "./components/FormValidation/FormWithoutMap";
+import FormWithMap from "./components/FormValidation/FormWithMap";
+import FetchingTime from "./components/FormValidation/FetchingTime";
+import FetchWithUpdate from "./components/FormValidation/FetchWithUpdate";
+import Map from "./components/CHARTS AND MAPS/Map";
 function App() {
   return (
     <div className="App">
@@ -61,7 +76,24 @@ function App() {
         <Route path="/otp" element={<ReactOtp />} />
         <Route path="/paginate" element={<Paginate />} />
         <Route path="/load" element={<LoadingPage />} />
-  
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/disable" element={<DisabledButton />} />
+        <Route exact path="/UserList" element={<UserList />} />
+        <Route path="/userPage/:id" element={<UserPage />} />
+        <Route exact path="/slider" element={<CardSlider />} />
+        <Route exact path="/MoviePage/:id" element={<MoviePage />} />
+        <Route exact path="/extract" element={<TitleExtraction />} />
+        <Route exact path="/map" element={<MapWithIcons />} />
+        <Route path="/MapWithIconPage/:page" element={<MapWithIconPage />} />
+        <Route path="table" element={<MapIntoTable />} />
+        <Route path="user/:fname" element={<MapIntoTableUserPage />} />
+        <Route path="quiz" element={<Quiz />} />
+        <Route path="quiz1" element={<Quiz1 />} />
+        <Route path="form" element={<FormWithoutMap />} />
+        <Route path="form1" element={<FormWithMap />} />
+        <Route path="fetch" element={<FetchingTime />} />
+        <Route path="update" element={<FetchWithUpdate />} />
+        <Route path="map1" element={<Map />} />
       </Routes>
     </div>
   );
